d8366fd7baa536025f3d0ac66c2b7f3463f84cb9
stanislike <s.joulah@outlook.fr>
2024-08-01 20:28:14 +0200

    Bubble Generator finished

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const bubble = document.createElement("span");
-bubble.classList.add("bubble");
-document.body.appendChild(bubble);
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
 
-const size = Math.random() * 200 + 100 + "px";
-bubble.style.height = size;
-bubble.style.width = size;
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
 
-bubble.style.top = Math.random() * 100 + 50 + "%";
-bubble.style.left = Math.random() * 100 + "%";
+  const size = Math.random() * 200 + 100 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
 
-bubble.style.setProperty("--left", Math.random() * 100 + "%");
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
 
-console.log(size);
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+  bubble.addEventListener("click", () => {
+    counter++;
+    counterDisplay.textContent = counter;
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 1000);
